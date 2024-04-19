51f86bc5b37ded946787db85655a3eddd516028c
Anne Hunt <acathunt@gmail.com>
2024-04-19 12:42:13 -0600

    forms filled out

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -6,7 +6,11 @@
 <template>
 <div>
     <form>
-        
+        <label class="form-label" for="postBody"></label>
+<input type="text-area" name="body" class="form-control" id="postBody" maxlength="5000" required>
+
+<label class="form-label" for="postImg"></label>
+<input type="url" name="imgUrl" class="form-control" id="postImg" maxlength="500">
     </form>
 </div>
 </template>
--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -7,34 +7,34 @@
 <div>
     <form @submit.prevent>
 <label class="form-label" for="updateEmail"></label>
-<input type="email" name="email" class="form-control" id="updateEmail">
+<input type="email" name="email" class="form-control" id="updateEmail" maxLength="500">
 
 <label class="form-label" for="updateName"></label>
-<input type="text" name="name" class="form-control" id="updateName">
+<input type="text" name="name" class="form-control" id="updateName" maxlength="100">
 
 <label class="form-label" for="updatePicture"></label>
-<input type="url" name="picture" class="form-control" id="updatePicture">
+<input type="url" name="picture" class="form-control" id="updatePicture" maxlength="500">
 
 <label class="form-label" for="updateBio"></label>
-<input type="text-area" name="bio" class="form-control" id="updateBio">
+<input type="text-area" name="bio" class="form-control" id="updateBio" maxlength="1000">
 
 <label class="form-label" for="updateCoverImg"></label>
-<input type="url" name="coverImg" class="form-control" id="updateCoverImg">
+<input type="url" name="coverImg" class="form-control" id="updateCoverImg" maxlength="500">
 
 <label class="form-label" for="updateGithub"></label>
-<input type="url" name="github" class="form-control" id="updateGithub">
+<input type="url" name="github" class="form-control" id="updateGithub" maxlength="500">
 
 <label class="form-label" for="updatedLinkedin"></label>
-<input type="url" name="linkedin" class="form-control" id="updateLinkedin">
+<input type="url" name="linkedin" class="form-control" id="updateLinkedin" maxlength="500">
 
 <label class="form-label" for="updateResume"></label>
-<input type="url" name="resume" class="form-control" id="updateResume">
+<input type="url" name="resume" class="form-control" id="updateResume" maxlength="500">
 
 <label class="form-label" for="updateGraduation"></label>
 <input type="checkbox" name="graduated" class="form-control" id="updateGraduation" unchecked>
 
 <label class="form-label" for="updateClass"></label>
-<input type="text" name="class" class="form-control" id="updateClass">
+<input type="text" name="class" class="form-control" id="updateClass" maxlength="100">
 
 <button type="submit" class="btn btn-primary">Submit Changes</button>
     </form>
